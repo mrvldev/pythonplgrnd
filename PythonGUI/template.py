682e13308ea42dcde6dab24ee06a255dc690e6e2
mrvldev <mikerawvonl@gmail.com>
2025-11-20 11:21:47 +0100

    followed yt tutorial

---
+++ PythonGUI/template.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+QVBoxLayout, QHBoxLayout
+
+#App Settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Template")
+main_window.resize(300,200)
+
+#Create all Object/Widgets below here
+
+main_window.show()
+app.exec_()
